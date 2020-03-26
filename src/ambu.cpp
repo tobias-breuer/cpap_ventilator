32466383d676233dd8869a606f95e3a671094ec0
Alvar Penning <post@0x21.biz>
2020-03-26 12:51:33 +0100

    Kind-of linear process for Ambu Bag
    Number of revolutions is not identical and must still be adjusted. The mode
    modifier is also still missing.

--- src/ambu.cpp
+++ src/ambu.cpp
@@ -5,9 +5,9 @@
 #include "./ambu.h"
 #include "./state.h"
 
-inline void ambu_motor_measure_cycle();
-long ambu_motor_read_stop();
-void ambu_motor_write_direction();
+inline long ambu_motor_state_read();
+void ambu_motor_write_direction(double);
+inline double ambu_state_fun(double);
 
 extern volatile float breaths_per_minute;
 
@@ -18,11 +18,16 @@ enum direction_t {
 };
 direction_t direction = forward;
 
-long breathing_cycle_ms = 0;
+double breath_time_s = 0;
 
 void ambu_loop() {
-  ambu_motor_read_stop();
-  ambu_motor_write_direction();
+  breaths_per_minute = min(max(breaths_per_minute, AMBU_MIN_CYCLE), AMBU_MAX_CYCLE);
+  breath_time_s = 60.0 / (breaths_per_minute);
+
+  const double breathing_cycle_s = (double) ambu_motor_state_read() / 1000.0;
+  const double speed = ambu_state_fun(breathing_cycle_s);  // TODO: read correct mode
+
+  ambu_motor_write_direction(speed);
 }
 
 void ambu_reset() {
@@ -33,68 +38,51 @@ void ambu_setup() {
   pinMode(PIN_MOTOR_IN3, OUTPUT);
   pinMode(PIN_MOTOR_IN4, OUTPUT);
   pinMode(PIN_MOTOR_STOP, INPUT_PULLUP);
-
-  ambu_motor_measure_cycle();
 }
 
-long ambu_motor_read_stop() {
+inline long ambu_motor_state_read() {
   static long last_stop = millis();
 
-  if (!digitalRead(PIN_MOTOR_STOP)) {
-    const long now = millis();
-    if (now - last_stop > MOTOR_STOP_DEBOUNCE_MS){
-      long breathing_cycle_ms = now - last_stop;
+  const long now = millis();
+  const long breathing_cycle_ms = now - last_stop;
 
-      last_stop = now;
-      direction = (direction_t) -direction;
+  const bool is_motor_stop = !digitalRead(PIN_MOTOR_STOP);
+  const bool is_motor_debounced = breathing_cycle_ms > MOTOR_STOP_DEBOUNCE_MS;
+  const bool is_timeout = breathing_cycle_ms >= (breath_time_s * 1000);
 
-      Serial.print("[info] reached stop pin after ");
-      Serial.print(breathing_cycle_ms);
-      Serial.print(" ms, setting direction ");
-      Serial.println(direction);
+  // update last_stop iff button is pressed and debounced or timeout is reached
+  if ((is_motor_stop && is_motor_debounced) || is_timeout) {
+    last_stop = now;
+    direction = (direction_t) -direction;
 
-      // increment cycle counter if we switched back to forward mode
-      if (direction == forward) {
-        Serial.print("[info] finished cycle count iteration number ");
-        Serial.println(cycle_count_increment());
-      }
+    Serial.print("[info] reached stop after ");
+    Serial.print(breathing_cycle_ms);
+    Serial.print(" ms, setting direction ");
+    Serial.println(direction);
 
-      return breathing_cycle_ms;
+    // increment cycle counter if we switched back to forward mode
+    if (direction == forward) {
+      Serial.print("[info] finished cycle count iteration number ");
+      Serial.println(cycle_count_increment());
     }
+
+    // after switching the mode, last_stop was updated and recalculating the
+    // breathing cycle results in zero.
+    return 0;
   }
 
-  return 0;
+  return breathing_cycle_ms;
 }
 
-inline void ambu_motor_measure_cycle() {
-  Serial.println("[info] measuring motor cycle duration");
-  ambu_motor_write_direction();
-
-  while (breathing_cycle_ms == 0) {
-    breathing_cycle_ms = ambu_motor_read_stop();
-  }
-  Serial.print("[info] first cycle took ");
-  Serial.print(breathing_cycle_ms);
-  Serial.println("ms");
-  breathing_cycle_ms = 0;
-
-  while (breathing_cycle_ms == 0) {
-    breathing_cycle_ms = ambu_motor_read_stop();
-  }
-  Serial.print("[info] a breathing cycle takes ");
-  Serial.print(breathing_cycle_ms);
-  Serial.println("ms");
-}
-
-void ambu_motor_write_direction() {
+void ambu_motor_write_direction(double speed) {
   switch (direction) {
     case forward:
       analogWrite(PIN_MOTOR_IN3, 0);
-      analogWrite(PIN_MOTOR_IN4, 255);
+      analogWrite(PIN_MOTOR_IN4, speed);
       break;
 
     case backward:
-      analogWrite(PIN_MOTOR_IN3, 255);
+      analogWrite(PIN_MOTOR_IN3, speed);
       analogWrite(PIN_MOTOR_IN4, 0);
       break;
 
@@ -103,6 +91,21 @@ void ambu_motor_write_direction() {
       analogWrite(PIN_MOTOR_IN3, 0);
       analogWrite(PIN_MOTOR_IN4, 0);
   }
+}
+
+inline double ambu_state_fun(double pos_s) {
+  // TODO mode_multi
+
+  const double factor = breath_time_s / AMBU_MIN_BREATH_S;
+
+  const int pt_min_y = 255 - (int) (255.0 / factor);
+  const int pt_max_x = AMBU_MIN_BREATH_S * factor;
+
+  const double gradient = (255.0 - pt_min_y) / (pt_max_x);
+
+  const double x_s = (direction == backward) ? pt_max_x - pos_s : pos_s;
+
+  return gradient * x_s + pt_min_y;
 }
 
 #endif  // MODE_AMBU
