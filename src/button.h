b4a9bc7db4eacacafeb41cff6cb4844fc86dd67a
Alvar Penning <post@0x21.biz>
2020-03-26 19:57:41 +0100

    Delay button execution within debounce_btn

--- src/button.h
+++ src/button.h
@@ -1,8 +1,9 @@
 #pragma once
 
 /**
- * A function-like macro to debounce code to be executed after the press of some
- * button/switch.
+ * A function-like macro to both delay and debounce code to be executed while
+ * pressing some button/switch. The button must be pressed for at least the
+ * configured time and is locked for the same time after being executed.
  *
  * The code can be used as follows:
  *
@@ -11,11 +12,11 @@
  *    }
  *
  * @param BTN address of the button
- * @param DEBOUNCE_MS debouncing time in ms
+ * @param DEBOUNCE_MS debouncing/lock time in ms
  */
 #define debounce_btn(BTN, DEBOUNCE_MS) \
-  static volatile long last_press ##BTN = 0; \
-  for ( \
-    ; \
-    !digitalRead(BTN) && (millis() - last_press ##BTN > DEBOUNCE_MS); \
-    last_press ##BTN = millis())
+  static volatile long last_press ##BTN = millis(); \
+  if (digitalRead(BTN)) { \
+    last_press ##BTN = millis(); \
+  } else \
+    for (; millis() - last_press ##BTN > DEBOUNCE_MS; last_press ##BTN = millis())
